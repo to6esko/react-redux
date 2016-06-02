d2caad47fe16bbb97fcfa820c16bab6c412b128e
Todor Krastev <to6esko69@gmail.com>
2016-06-02 10:49:16 +0300

    remove comments

--- learn-redux/client/components/Comments.js
+++ learn-redux/client/components/Comments.js
@@ -8,19 +8,28 @@ const Comments = React.createClass({
                     <strong>
                         {comment.text}
                     </strong>
-                    <button className="remove-comment">&times;
+                    <button className="remove-comment"
+                        onClick={this.props.removeComment.bind(null,this.props.params.postId, i) }>&times;
                         </button>
                     </p>
                 </div>
         )
     },
+    handleSubmit(e) {
+        e.preventDefault();
+        const {postId} = this.props.params;
+        const author = this.refs.author.value;
+        const comment = this.refs.comment.value;
+        this.props.addComment(postId, author, comment);
+        this.refs.commentForm.reset();
+    },
     render() {
         return (
             <div className="comments">
                 {this.props.postComments.map(this.renderComments) }
-                <form ref="commentForm" className="comment-form">
-                   <input type="text" ref="author" placeholder="author"/>
-                   <input type="text" ref="comment" placeholder="comment"/>
+                <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
+                    <input type="text" ref="author" placeholder="author"/>
+                    <input type="text" ref="comment" placeholder="comment"/>
                     <input type="submit" hidden/>
                 </form>
           </div>      
