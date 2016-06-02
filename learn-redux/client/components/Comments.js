bfde452262a7b690ad1e6e4c95956f431bc9d77b
Todor Krastev <to6esko69@gmail.com>
2016-06-02 09:59:49 +0300

    add comments box

---
+++ learn-redux/client/components/Comments.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Comments = React.createClass({
+    renderComments(comment, i) {
+        return (
+            <div className="comment" key={i}>
+                <p>
+                    <strong>
+                        {comment.text}
+                    </strong>
+                    <button className="remove-comment">&times;
+                        </button>
+                    </p>
+                </div>
+        )
+    },
+    render() {
+        return (
+            <div className="comments">
+                {this.props.postComments.map(this.renderComments) }
+                <form ref="commentForm" className="comment-form">
+                   <input type="text" ref="author" placeholder="author"/>
+                   <input type="text" ref="comment" placeholder="comment"/>
+                    <input type="submit" hidden/>
+                </form>
+          </div>      
+      )
+    }
+});
+export default Comments;
